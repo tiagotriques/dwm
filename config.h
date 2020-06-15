fb003cde4338ffe28822bead51b999490ea52153
Tiago Esperança Triques <tiago.triques@gmail.com>
2020-06-15 11:37:41 -0300

    Adiciona patch noborder e reconfigura swallow

--- config.h
+++ config.h
@@ -24,20 +24,21 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
+	 * WM_CLASS(STRING) = instance, class
+	 * WM_NAME(STRING) = title
 	 */
 	/* class            , instance , title          , tags mask , isfloating , isterminal , noswallow , monitor */
-	{ "Gimp"            , NULL     , NULL           , 0         , 1          , 0          , 0         , -1 },
-	{ "Firefox"         , NULL     , NULL           , 0         , 0          , 0          , 0         , -1 },
-	{ "Alacritty"       , NULL     , NULL           , 0         , 0          , 1          , 1         , -1 },
-	{ "St"              , NULL     , NULL           , 0         , 0          , 1          , 0         , -1 },
+	{ "Gimp"            , NULL     , NULL           , 0         , 0          , 0          , 0         , -1 },
+	{ "Firefox"         , NULL     , NULL           , 1 << 0    , 0          , 0          , 0         , -1 },
+	{ "Alacritty"       , NULL     , NULL           , 0         , 0          , 0          , 0         , -1 },
+	{ "st"              , "st"     , "st"           , 0         , 0          , 1          , 0         , -1 },
 	{ NULL              , NULL     , "Event Tester" , 0         , 1          , 0          , 0         , -1 }, /* xev */
+	{ NULL              , NULL     , "stns"         , 0         , 0          , 0          , 0         , -1 }, /* xev */
     { "TelegramDesktop" , NULL     , "Telegram"     , 1 << 8    , 0          , 0          , 0         , -1 }
 };
 
 /* layout(s) */
-static const float mfact     = 0.60; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -65,6 +66,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *termnscmd[]  = { "st", "-t", "stns", NULL };
 static const char *browsercmd[] = {"firefox", NULL };
 static const char *ncmpcppcmd[] = {"st", "-e", "ncmpcpp", NULL };
 static const char *printscreencmd[] = {"/usr/local/bin/screenshot", NULL };
@@ -73,17 +75,18 @@ static const char *printscreencmd[] = {"/usr/local/bin/screenshot", NULL };
 
 static Key keys[] = {
 	/* modifier             key                         function        argument */
-    { 0,                    XF86XK_AudioMute,           spawn,          SHCMD("pamixer -t;pkill -$((34+12)) dwmblocks") },
-    { 0,                    XF86XK_AudioLowerVolume,    spawn,          SHCMD("pamixer -d 1;pkill -$((34+12)) dwmblocks") },
-    { 0,                    XF86XK_AudioRaiseVolume,    spawn,          SHCMD("pamixer -i 1;pkill -$((34+12)) dwmblocks") },
-    { 0,                    XF86XK_MonBrightnessUp,     spawn,          SHCMD("xbacklight -inc 10;pkill -$((34+11)) dwmblocks") },
-    { 0,                    XF86XK_MonBrightnessDown,   spawn,          SHCMD("xbacklight -dec 10;pkill -$((34+11)) dwmblocks") },
-    { 0,                    XF86XK_AudioPlay,           spawn,          SHCMD("mpc toggle") },
-    { 0,                    XF86XK_AudioNext,           spawn,          SHCMD("mpc next") },
-    { 0,                    XF86XK_AudioPrev,           spawn,          SHCMD("mpc prev") },
+	{ 0,                    XF86XK_AudioMute,           spawn,          SHCMD("pamixer -t; pkill -$((34+12)) dwmblocks") },
+	{ 0,                    XF86XK_AudioLowerVolume,    spawn,          SHCMD("pamixer -d 5; pkill -$((34+12)) dwmblocks") },
+	{ 0,                    XF86XK_AudioRaiseVolume,    spawn,          SHCMD("pamixer -i 5; pkill -$((34+12)) dwmblocks") },
+	{ 0,                    XF86XK_MonBrightnessUp,     spawn,          SHCMD("xbacklight -inc 10; pkill -$((34+11)) dwmblocks") },
+	{ 0,                    XF86XK_MonBrightnessDown,   spawn,          SHCMD("xbacklight -dec 10; pkill -$((34+11)) dwmblocks") },
+	{ 0,                    XF86XK_AudioPlay,           spawn,          SHCMD("mpc toggle") },
+	{ 0,                    XF86XK_AudioNext,           spawn,          SHCMD("mpc next") },
+	{ 0,                    XF86XK_AudioPrev,           spawn,          SHCMD("mpc prev") },
 	{ 0,                    XK_Print,                   spawn,          {.v = printscreencmd } },
 	{ MODKEY,               XK_p,                       spawn,          {.v = dmenucmd } },
 	{ MODKEY,               XK_Return,                  spawn,          {.v = termcmd } },
+	{ ControlMask,          XK_Return,                  spawn,          {.v = termnscmd } },
 	{ MODKEY,               XK_w,                       spawn,          {.v = browsercmd } },
 	{ MODKEY,               XK_n,                       spawn,          {.v = ncmpcppcmd } },
 	{ MODKEY,               XK_b,                       togglebar,      {0} },
@@ -103,13 +106,13 @@ static Key keys[] = {
 	{ MODKEY,               XK_o,                       setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,               XK_space,                   setlayout,      {0} },
 	{ MODKEY|ShiftMask,     XK_space,                   togglefloating, {0} },
-    { MODKEY,               XK_0,                       view,           {.ui = ~0 } },
-    { MODKEY|ShiftMask,     XK_0,                       tag,            {.ui = ~0 } },
+	{ MODKEY,               XK_0,                       view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,     XK_0,                       tag,            {.ui = ~0 } },
 	{ MODKEY,               XK_comma,                   focusmon,       {.i = -1 } },
 	{ MODKEY,               XK_period,                  focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,     XK_comma,                   tagmon,         {.i = -1 } },
-    { MODKEY|ShiftMask,     XK_period,                  tagmon,         {.i = +1 } },
-    TAGKEYS(                XK_1,                                       0)
+	{ MODKEY|ShiftMask,     XK_period,                  tagmon,         {.i = +1 } },
+	TAGKEYS(                XK_1,                                       0)
 	TAGKEYS(                XK_2,                                       1)
 	TAGKEYS(                XK_3,                                       2)
 	TAGKEYS(                XK_4,                                       3)
