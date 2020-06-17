5d6a050e071cce7b0301c6c2c2575589e8f39b09
Tiago Esperança Triques <tiago.triques@gmail.com>
2020-06-17 16:51:54 -0300

    Commit depois da caca dar certo :)
    patch de systray adicionado

--- config.h
+++ config.h
@@ -1,9 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */                                                                                     static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/static const unsigned int snap      = 32;       /* snap pixel */
+static const int showsystray        = 1;     /* 0 means no systray */                                                                                     static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
